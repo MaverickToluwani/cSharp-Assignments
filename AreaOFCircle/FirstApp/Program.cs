0b48806efb4999b0b7ac67f27e1a2086465608d6
Mike <mickelegend089@gmail.com>
2025-02-28 07:54:28 +0100

    new add

---
+++ AreaOFCircle/FirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AreaOfCircle
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 22;
+            Double result = AreaOfCircle(r);
+            Console.WriteLine(result);
+        }
+
+
+        static double AreaOfCircle(double radius)
+        {
+            const double PI = Math.PI;
+
+            double AreaOFCircle = radius * PI;
+
+            return AreaOFCircle;
+        }
+    }
+}
+
---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace SimpleCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Simple Calculator");
+
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator (+, -, *, /): ");
+            char op = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            SimpleCalculator(num1, num2, op);
+
+        }
+
+
+        static void SimpleCalculator(double FirstValue, double SecondValue, char Operation)
+        {
+            double result = 0;
+
+            if (Operation == '+')
+            {
+                result = FirstValue + SecondValue;
+            }
+            else if (Operation == '-')
+            {
+                result = FirstValue - SecondValue;
+            }
+            else if (Operation == '*')
+            {
+                result = FirstValue * SecondValue;
+            }
+            else if (Operation == '/')
+            {
+                result = FirstValue / SecondValue;
+            }
+            else
+            {
+                Console.WriteLine($"{Operation} is invalid");
+            }
+
+             Console.WriteLine($"Result: {FirstValue} {Operation} {SecondValue} = {result}");
+            }
+        }
+
+    }
+
