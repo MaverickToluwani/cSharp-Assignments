ba160f769d1fd033983ce69278f351ace0ad65fe
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2025-03-08 17:11:00 +0100

    BlogApi

---
+++ Toluwani_Blog/BlogApi/Program.cs
@@ -0,0 +1,46 @@
+using BusinessLogicLayer.CategoryService;
+using BusinessLogicLayer.IService;
+using BusinessLogicLayer.MapperMethods;
+using BusinessLogicLayer.Service;
+using DataAccessLayer.Data;
+using DataAccessLayer.IRepositories;
+using DataAccessLayer.Repositries;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategory, CategoryRepository>();
+builder.Services.AddScoped<IProduct, ProductRepository>();
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<CategoryMapper>();
+builder.Services.AddScoped<UserMapper>();
+
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
